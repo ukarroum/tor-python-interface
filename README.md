936510abefe10353385ad4b502bffa66b31597bb
Yassir Karroum <ukarroum17@gmail.com>
2016-07-30 11:27:57 +0100

    Commit initiale

--- README.md
+++ README.md
@@ -1,2 +1,29 @@
-# tor-python-interface
-Une interface pour une utilisation rapide de tor par python.
+# Interface Tor Python
+
+Un petit script qui permet de facilement utiliser tor avec python.
+
+# Technologies Utilisés
+
+* [Beautiful Soup](https://www.crummy.com/software/BeautifulSoup/bs4/doc/): Une bibliothéque de scrapping qui permet de naviguer 
+facilement dans une page html pour la récupération de l'ip.
+* [Stem](https://stem.torproject.org/) : Une bibliotehque qui permet d'intégrer TOR à Python.
+* [PyCUrl](http://pycurl.io/): Une bibliotehque pour récuperer le code HTML des pages, URLLib m'a posé pas mal de problèmes pour passer par TOR.
+
+# Dependances
+
+``` 
+# apt-get install python3-bs4
+# pip3 install stem
+# apt-get install python3-pycurl
+# apt-get install tor
+```
+
+# Fonctions
+
+
+* **init_tor** : Permet d'initialiser tor en le lancant.
+* **kill_tor** : Met fin au processus de TOR.
+* **get_html** : Récupere le code HTML d'une page passé en parametre en passant par TOR
+* **new_identity** : Permet de modifier votre adresse ip (en fait le changement de circuit ne change pas foércément
+                        l'ip mais le nombre des noeuds de sorties est trés grand donc celà change généralement votre ip)
+* **get_ip**   : Récupere votre ip.
---
+++ tor_python_interface.py
@@ -0,0 +1,97 @@
+""""
+ * ************* Interface Tor Python ***************
+ *
+ * Ecrit par Yassir Karroum [ukarroum17@gmail.com] [https://github.com/ukarroum]
+ * Le 30 juillet 2016
+ *
+ * Un simple script qui permet d'utiliser tor facilement avec python.
+ * Il propose plusieurs fonctions :
+
+        - init_tor : Permet d'initialiser tor en le lancant.
+        - kill_tor : Met fin au processus de TOR.
+        - get_html : Récupere le code HTML d'une page passé en parametre en passant par TOR
+        - new_identity : Permet de modifier votre adresse ip (en fait le changement de circuit ne change pas foércément
+                        l'ip mais le nombre des noeuds de sorties est trés grand donc celà change généralement votre ip)
+        - get_ip   : Récupere votre ip.
+
+    Le script nécéssite plusieurs dépendances : Beautiful Soup, Stem, PYCurl et tor.
+    Ils peuvent etre installés via ces 4 commandes (sous debian et dérivés) :
+
+    # apt-get install python3-bs4
+    # pip3 install stem
+    # apt-get install python3-pycurl
+    # apt-get install tor
+
+ */
+"""
+
+import stem.process
+from stem.util import term
+import pycurl
+import io
+from bs4 import BeautifulSoup
+
+SOCKS_PORT = 7000
+
+tor_process = 0
+use_tor = False   # Si tor est utilisé le parametre devient true
+
+
+def init_tor():
+
+    global tor_process
+    tor_process = stem.process.launch_tor_with_config(
+        config={
+            'SocksPort': str(SOCKS_PORT)
+        }
+    )
+
+    global use_tor
+    use_tor = True
+
+
+def kill_tor():
+
+    tor_process.kill()
+
+
+def get_html(url):
+    """
+    Uses pycurl to fetch a site using the proxy on the SOCKS_PORT.
+    """
+
+    output = io.BytesIO()
+
+    query = pycurl.Curl()
+    query.setopt(pycurl.URL, url)
+
+    if use_tor:
+        query.setopt(pycurl.PROXY, 'localhost')
+        query.setopt(pycurl.PROXYPORT, SOCKS_PORT)
+        query.setopt(pycurl.PROXYTYPE, pycurl.PROXYTYPE_SOCKS5_HOSTNAME)
+
+    query.setopt(pycurl.USERAGENT, 'Mozilla/5.0')
+    query.setopt(pycurl.WRITEFUNCTION, output.write)
+
+    try:
+        query.perform()
+        return output.getvalue()
+    except pycurl.error as exc:
+        return "Unable to reach %s (%s)" % (url, exc)
+
+
+def new_identity():
+    if use_tor:
+        print(term.format("Nouvelle identite : "+get_ip()+"\n", term.Color.BLUE))
+        kill_tor()
+        init_tor()
+    else:
+        print(term.format("Lancement de Tor.\n", term.Attr.BOLD))
+        init_tor()
+
+
+def get_ip():
+
+    soup = BeautifulSoup(get_html("http://whatismyipaddress.com/"), "html.parser")
+
+    return soup.find(id="section_left").find_all('div')[1].a.string
